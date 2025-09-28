37a431e64272bce14e94987281fa042568a5d6fa
brijesh131313 <bg8000040@gmail.com>
2025-09-28 09:33:31 +0545

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'smartstore-v1.0';
+const urlsToCache = [
+  './deep.html',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
